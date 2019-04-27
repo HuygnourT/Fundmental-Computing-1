60ea3bcf1fa0cd18a288a8899a7e8ba043b4b141
Truong Huy <phuy0212@gmail.com>
2019-04-27 20:45:45 +0700

    Add instanceof

--- src/candidate/ArrayOfEmployee.java
+++ src/candidate/ArrayOfEmployee.java
@@ -15,10 +15,9 @@ public class ArrayOfEmployee {
 			int choose = in.nextInt();
 			Employee tmp = new Employee();
 			if(choose == 1)
-				tmp.input();
+				tmp = new Regular();
 			else if (choose == 2)
 				tmp = new Contract();
-			
 			tmp.input();
 			list[i] = tmp;
 		}
@@ -32,10 +31,20 @@ public class ArrayOfEmployee {
 			list[i].output();
 		}
 	}
+	public void count()
+	{
+		int c1=0,c2=0;
+		for(int i =0 ; i < n ; i++)
+			if(list[i] instanceof Regular)
+				c1++;
+			else c2++;
+		System.out.println("The number of regular employee is "+c1+"\nThe number of contract employee is "+c2);
+	}
 	public static void main(String[] args) {
 		ArrayOfEmployee a = new ArrayOfEmployee();
 		a.input();
 		a.output();
+		a.count();
 	}
 
 }
